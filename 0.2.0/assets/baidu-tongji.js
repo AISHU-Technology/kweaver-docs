3e84772e5f0bc03a9fc92c1a830ad1c3b3fb492f
clz <876812297@qq.com>
2024-07-16 18:52:53 +0800

    Deployed 056a63f to 0.2.0 with MkDocs 1.6.0 and mike 2.1.2

---
+++ 0.2.0/assets/baidu-tongji.js
@@ -0,0 +1,16 @@
+//
+// var _hmt = _hmt || [];
+// (function() {
+//     var hm = document.createElement("script");
+//     hm.src = "https://hm.baidu.com/hm.js?01ab0a1f2282b376242dcee06898cd2e";
+//     var s = document.getElementsByTagName("script")[0];
+//     s.parentNode.insertBefore(hm, s);
+// })();
+
+var _hmt = _hmt || [];
+(function() {
+    var hm = document.createElement("script");
+    hm.src = "https://hm.baidu.com/hm.js?93fc156e115399cbe35fc3168519e77a";
+    var s = document.getElementsByTagName("script")[0];
+    s.parentNode.insertBefore(hm, s);
+})();
